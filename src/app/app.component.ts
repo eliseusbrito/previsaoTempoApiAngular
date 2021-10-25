6f407a7b4dfe24112afc9f1105513056f02c737b
eliseusbrito <eliseusbrito@gmail.com>
2021-10-25 06:59:49 -0300

    Adiciona timezone e altera app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as moment from 'moment-timezone';
+import 'moment/locale/pt-br';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'previsao-tempo';
+
+  constructor() {
+    moment.locale('pt-br');
+  }
+
 }
